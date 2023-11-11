d25bcea5fe6eb68158241fde2ddc8552e7fd4df8
Pedro vyctor <pedro.vyctor00@gmail.com>
2023-11-11 19:06:08 -0300

    Ainda em faze de construção da ordenação da lista de clientes

--- bibli/Projeto 2.c
+++ bibli/Projeto 2.c
@@ -150,7 +150,58 @@ Cliente* Aux_Cliente(Cliente *list, int Cd_Cliente, char nome[])
     return novo;
 }
 
+Cliente* Ordenamento_Cliente (Cliente *cliente)
+{
+    Cliente *cliente1, *aux1;
+    Livros_Disponiveis *aux2;
+    int identificador;
+    cliente1 = NULL;
+    aux1 = NULL;
 
+    if(cliente->prox != NULL)
+    {
+        do
+        {
+            identificador = 0;
+            for(; cliente != NULL; cliente = cliente->prox)
+            {
+                if(aux1 != NULL)
+                {
+                    if(strcmp(cliente->Nome, aux1->Nome) <= 0)
+                    {
+                        
+                    }
+                }
+                else
+                {
+                    if(strcmp(cliente->prox->Nome, cliente->Nome) <= 0)
+                    {
+                        identificador++;
+                        cliente1 = Aux_Cliente(cliente1, cliente->prox->cd_Identificacao, cliente->prox->Nome);
+                        aux2 = cliente->prox->Emprestimo;
+                        for(; aux2 != NULL; aux2 = aux2->prox)
+                        {
+                            cliente1->Emprestimo = Inputar_Disponivel_Ordenadamente(cliente1->Emprestimo, aux2->cd_registro, aux2->Titulo, aux2->Assunto, aux2->Autor);
+                        }
+                        aux1 = Aux_Cliente(aux1, cliente->cd_Identificacao, cliente->Nome);
+                        aux2 = cliente->Emprestimo;
+                        for(; aux2 != NULL; aux2 = aux2->prox)
+                        {
+                            aux1->Emprestimo = Inputar_Disponivel_Ordenadamente(aux1->Emprestimo, aux2->cd_registro, aux2->Titulo, aux2->Assunto, aux2->Autor);
+                        }
+                        cliente = cliente->prox;
+                        cliente = cliente->prox;
+                    }
+                }                
+            }
+        } while (identificador != 0);
+        
+    }
+    else
+    {
+        return cliente;
+    }
+}
 
 //A função abaixo identifica se o livro digitado pelo usuário, já foi anteriormente cadastrado.
 int Identificador_livro_existente(Livros_Disponiveis *list, char titulo[])
